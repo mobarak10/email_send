7ee88670b30bb3456ced53035f98f493bd27ccbf
mobarak10 <47492246+mobarak10@users.noreply.github.com>
2019-04-08 01:09:00 +0600

    Update one_mail.php

--- one_mail.php
+++ one_mail.php
@@ -88,8 +88,8 @@ if(isset($_FILES['image'])){
         $mail->isSMTP();                                            // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';    // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                                     // Enable SMTP authentication
-        $mail->Username = 'mobarak.hossain.cse12@gmail.com';                 // SMTP username
-        $mail->Password = 'joykhan151390';                              // SMTP password
+        $mail->Username = 'yourgmail@.com';                 // SMTP username
+        $mail->Password = 'Your Password';                              // SMTP password
         $mail->SMTPSecure = 'TLS';                                  // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                          // TCP port to connect to
 
